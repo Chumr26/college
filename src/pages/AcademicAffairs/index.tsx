c648a277d62830ee94a35535781e55c043838210
Chumr26 <nguyenanhkhoa26092002@gmail.com>
2025-10-09 15:07:00 +0700

    not important

--- src/pages/AcademicAffairs/index.tsx
+++ src/pages/AcademicAffairs/index.tsx
@@ -18,7 +18,7 @@ export function AcademicAffairs({
             {/* Available Majors Accordion */}
             <section className="bg-gray-50 border-b-4 border-black py-20 dark:bg-gray-800">
                 <div className="container mx-auto px-4">
-                    <h2 className="text-5xl md:text-7xl tracking-tighter uppercase mb-16 text-center">
+                    <h2 className="text-5xl md:text-7xl tracking-tighter uppercase mb-16 text-center dark:text-gray-100">
                         NGÀNH
                         <br />
                         <span className="bg-red-500 text-white px-2">
